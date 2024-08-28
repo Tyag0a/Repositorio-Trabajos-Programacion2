fa1a7d59a80e637db78308473470b388ab6bcf35
Tyag0a <santiaguirreo26@gmail.com>
2024-08-27 22:55:07 -0500

    Diagrama de clases

--- co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/poo/MainBiblioteca.java
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/poo/MainBiblioteca.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.poo;
 
+import co.edu.uniquindio.poo.Biblioteca.model.Biblioteca;
+
 public class MainBiblioteca {
     public static void main(String[] args) {
-
+        Biblioteca menu = new Biblioteca();
+        menu.mostrarMenu();
     }
+
 }
